a3bbb72698c10e9cd2c11c25832d47d23c5ea900
matejcik <ja@matejcik.cz>
2023-03-10 12:23:20 +0100

    fixup! fix(common): deal with duplicity code leftovers

--- common/tools/coin_info.py
+++ common/tools/coin_info.py
@@ -370,12 +370,8 @@ def _load_builtin_ethereum_networks() -> Coins:
     chains_data = load_json("ethereum", "networks.json")
     networks: Coins = []
     for chain_data in chains_data:
-        chain_id = chain_data["chain_id"]
-        chain_data.update(
-            chain_id=chain_id,
-            key=f"eth:{chain_data['shortcut']}:{chain_id}",
-            # is_testnet is present in the JSON
-        )
+        chain_data["key"] = f"eth:{chain_data['shortcut']}:{chain_data['chain_id']}"
+        # is_testnet is present in the JSON
         networks.append(cast(Coin, chain_data))
 
     return networks
